9845f48151596c3287b8ddfaf015674d4d7a6b4f
ReubenBidoof <rubenhoenderken@gmail.com>
2025-04-15 11:25:30 +0200

    more chnages for unicorn

--- Assets/Codes/BookPlane.cs
+++ Assets/Codes/BookPlane.cs
@@ -1,5 +1,7 @@
+using System.Runtime.CompilerServices;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class BookPlane : MonoBehaviour
 {
@@ -7,14 +9,16 @@ public class BookPlane : MonoBehaviour
     public GameObject PrefabTwo;
     public Transform spawnPoint;    // optional: where to spawn it
     public bool bookopened = false;
+    private int spawnDistance = 3;
+    private Vector3 position; // Changed from int to Vector3 to fix the error
+
     void Update()
     {
+        position = new Vector3(transform.position.x + spawnDistance, transform.position.y, transform.position.z);
         if (Input.GetKeyDown(KeyCode.B) && bookopened == false)
         {
-           
             SpawnPlane();
             bookopened = true;
-         
         }
         else if (Input.GetKeyDown(KeyCode.B) && bookopened == true)
         {
@@ -25,11 +29,13 @@ public class BookPlane : MonoBehaviour
 
     void SpawnPlane()
     {
-        Vector3 position = spawnPoint ? spawnPoint.position : Vector3.zero;
-        PrefabTwo = Instantiate(planePrefab, position, Quaternion.identity);
+        Quaternion yOnlyRotation = Quaternion.Euler(0f, transform.eulerAngles.y, 0f);
+        Vector3 position = spawnPoint ? spawnPoint.position : Vector3.zero; // Fixed: position is now a Vector3
+        PrefabTwo = Instantiate(planePrefab, position, yOnlyRotation); // Fixed: position is now a Vector3
     }
+
     void DeletePlane()
     {
-            Destroy(PrefabTwo);
+        Destroy(PrefabTwo);
     }
 }
