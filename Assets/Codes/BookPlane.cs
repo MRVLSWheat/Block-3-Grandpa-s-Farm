0e4756e4b11f12262453ae1172b8b6cec90fb8a8
ReubenBidoof <rubenhoenderken@gmail.com>
2025-04-15 12:05:04 +0200

    Moresmallupdates

--- Assets/Codes/BookPlane.cs
+++ Assets/Codes/BookPlane.cs
@@ -1,41 +1,49 @@
-using System.Runtime.CompilerServices;
-using Unity.VisualScripting;
 using UnityEngine;
-using UnityEngine.UIElements;
 
 public class BookPlane : MonoBehaviour
 {
-    public GameObject planePrefab;  // assign your prefab in the Inspector
-    public GameObject PrefabTwo;
-    public Transform spawnPoint;    // optional: where to spawn it
-    public bool bookopened = false;
-    private int spawnDistance = 3;
-    private Vector3 position; // Changed from int to Vector3 to fix the error
+    public GameObject planePrefab;      // Assign your prefab in the Inspector
+    public GameObject spawnedPlane;     // To hold the spawned instance
+    public Transform cameraTransform;   // Assign the first person camera in the Inspector
+    public Vector3 offsetFromCamera = new Vector3(0f, -0.2f, 1.5f); // Adjust as needed
+    public Vector3 rotationOffset = new Vector3(0f, 180f, 90f);     // Fix sideways rotation
+    public bool bookOpened = false;
 
     void Update()
     {
-        position = new Vector3(transform.position.x + spawnDistance, transform.position.y, transform.position.z);
-        if (Input.GetKeyDown(KeyCode.B) && bookopened == false)
+        if (Input.GetKeyDown(KeyCode.B))
         {
-            SpawnPlane();
-            bookopened = true;
-        }
-        else if (Input.GetKeyDown(KeyCode.B) && bookopened == true)
-        {
-            DeletePlane();
-            bookopened = false;
+            if (!bookOpened)
+            {
+                SpawnPlane();
+                bookOpened = true;
+            }
+            else
+            {
+                DeletePlane();
+                bookOpened = false;
+            }
         }
     }
 
     void SpawnPlane()
     {
-        Quaternion yOnlyRotation = Quaternion.Euler(0f, transform.eulerAngles.y, 0f);
-        Vector3 position = spawnPoint ? spawnPoint.position : Vector3.zero; // Fixed: position is now a Vector3
-        PrefabTwo = Instantiate(planePrefab, position, yOnlyRotation); // Fixed: position is now a Vector3
+        // Instantiate and parent to camera
+        spawnedPlane = Instantiate(planePrefab);
+        spawnedPlane.transform.SetParent(cameraTransform);
+
+        // Set position relative to camera
+        spawnedPlane.transform.localPosition = offsetFromCamera;
+
+        // Set rotation to face player correctly
+        spawnedPlane.transform.localRotation = Quaternion.Euler(rotationOffset);
     }
 
     void DeletePlane()
     {
-        Destroy(PrefabTwo);
+        if (spawnedPlane != null)
+        {
+            Destroy(spawnedPlane);
+        }
     }
 }
