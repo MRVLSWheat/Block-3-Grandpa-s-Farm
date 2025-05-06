76f4eaa66cc75aef899d169db0e88b46ca8d6d4a
GNXaknu <aknu@ziggo.nl>
2025-05-06 19:44:56 +0200

    Quick patch for text

--- Assets/Scripts/Quests/QuestGiver.cs
+++ Assets/Scripts/Quests/QuestGiver.cs
@@ -5,9 +5,21 @@ public class QuestGiver : MonoBehaviour {
     bool playerInRange;
 
     void Update() {
-        if (playerInRange && Input.GetKeyDown(KeyCode.E)) {
+        if (!playerInRange || !Input.GetKeyDown(KeyCode.E)) return;
+
+        // If quest not started, start it
+        if (!QuestManager.Instance.activeQuests.Exists(q => q.questID == quest.questID)) {
             QuestManager.Instance.StartQuest(quest);
             Debug.Log("Started quest: " + quest.questTitle);
+            return;
+        }
+
+        // If find step complete, complete talk step
+        int talkIndex = quest.objectives.FindIndex(o => o.type == ObjectiveType.Talk);
+        int progress = QuestManager.Instance.GetProgress(quest.questID, talkIndex);
+        if (progress < quest.objectives[talkIndex].requiredAmount) {
+            QuestManager.Instance.ReportProgress(quest.questID, talkIndex, 1);
+            Debug.Log("Returned and talked to quest giver: " + quest.questTitle);
         }
     }
 
